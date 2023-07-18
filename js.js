703e51a6608827be6e04bac0e679cf27bc66bd7d
A-alturki <aboody3553@outlook.com>
2023-07-18 03:30:48 +0300

    added other colors

--- js.js
+++ js.js
@@ -1,17 +1,7 @@
-
-const slider = document.querySelector('.slider');
-slider.addEventListener("input", function(){
-        const container = document.querySelector('.container');
-        console.log(slider.value);
-        container.textContent = '';
-        container.remove();
-        gridMaker(slider.value);
-})
-
-
 function gridMaker(silderValue){
         const container = document.createElement('div');
         container.classList.add("container");
+        container.classList.add("rounded-border-gradient");
         const main = document.querySelector('.main');
 
 for (var i = 0; i < silderValue; ++i) {
@@ -25,24 +15,72 @@ for (var i = 0; i < silderValue; ++i) {
             column.appendChild(row); // append row in column
         }
         container.appendChild(column); // append column inside container
-    }
+}
 main.appendChild(container);
 
 
-
-var messages = document.querySelectorAll(".square");
-for (var i = 0; i < messages.length; i++) {
-        messages[i].addEventListener("mouseover", e=> {
-                
-                e.target.style.background = "#5885AF";
-                
-                
-        });
-}
 
 let sliderText = document.querySelector(".slider-text");
 sliderText.textContent = slider.value + "x" + slider.value;
 
 }
 
+function colorSelector(color){
+        var messages = document.querySelectorAll(".square");
+        for (var i = 0; i < messages.length; i++) {
+        messages[i].addEventListener("mouseover", e=> {
+                
+                e.target.style.background = color;
+                console.log(color);
+                
+        });
+}
+}
+
+
+
+const slider = document.querySelector('.slider');
+slider.addEventListener("input", function(){
+        const container = document.querySelector('.container');
+        console.log(slider.value);
+        container.textContent = '';
+        container.remove();
+        gridMaker(slider.value);
+})
+
+
 gridMaker(slider.value);
+
+
+document.querySelector('.button').onmousemove = (e) => {
+
+	const x = e.pageX - e.target.offsetLeft
+	const y = e.pageY - e.target.offsetTop
+
+	e.target.style.setProperty('--x', `${ x }px`)
+	e.target.style.setProperty('--y', `${ y }px`)
+	
+}
+
+
+
+let blue = document.querySelector(".blue");
+blue.addEventListener("click", function(){
+        color = "#41729F";
+        console.log("blue!");
+        colorSelector(color);
+})
+
+let cyan = document.querySelector(".cyan");
+cyan.addEventListener("click", function(){
+        color = "#47b8b3";
+        console.log("cyan!");
+        colorSelector(color);
+})
+
+let purple = document.querySelector(".purple");
+purple.addEventListener("click", function(){
+        color = "#474cb8";
+        console.log("purple!");
+        colorSelector(color);
+})
